3b95a9782a61f99d362605c4ae3499e4b547c3d8
Massimo Hamilton <massimo.hamilton@outlook.com>
2017-01-21 07:42:44 +0000

    Some cleanup of dependencies:
    - Move @types/node to root as it's shared between the front-end and back-end
    - Move @types/selenium-webdriver to root as it's used by the end to end tests
    - Move E2E tests to shared (as they are not front-end specific)
    - Remove node-fetch and use request-promise instead
    - Remove angular-in-memory-web-api which is for the Angular tutorial project
    - Remove lodash, canonical-path and rimraf which are not being used

--- back-end/specs/items.spec.ts
+++ back-end/specs/items.spec.ts
@@ -1,22 +1,22 @@
 import { expect } from 'chai';
-import fetch from 'node-fetch';
+import * as rp from 'request-promise';
 import catalogue from '../static-data/catalogue';
 
 describe('Fetch items', function () {
     this.timeout(20000);
 
     it('Should fetch the pizzas as expected', async () => {
-        let json = await (await fetch('http://localhost:3000/api/items/pizzas')).json();
+        let json = await rp.get('http://localhost:3000/api/items/pizzas', { json: true });
         expect(json).to.deep.equal(catalogue.pizzas);
     });
 
     it('Should fetch the drinks as expected', async () => {
-        let json = await (await fetch('http://localhost:3000/api/items/drinks')).json();
+        let json = await rp.get('http://localhost:3000/api/items/drinks', { json: true });
         expect(json).to.deep.equal(catalogue.drinks);
     });
 
     it('Should fetch the sides as expected', async () => {
-        let json = await (await fetch('http://localhost:3000/api/items/sides')).json();
+        let json = await rp.get('http://localhost:3000/api/items/sides', { json: true });
         expect(json).to.deep.equal(catalogue.sides);
     });
 });
