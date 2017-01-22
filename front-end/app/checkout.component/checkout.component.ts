411a8167055b5f26299490d642c5e26b6f94884e
SMH110 <mahdihassan135@hotmail.com>
2017-01-22 17:20:54 +0000

    Add BuyerService

--- front-end/app/checkout.component/checkout.component.ts
+++ front-end/app/checkout.component/checkout.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
 
 import { BasketService } from '../service/basket.service';
+import { BuyerDetailsService } from '../service/buyer-details.service';
 import { GuardService } from '../service/guard.service';
 
 @Component({
@@ -16,7 +16,7 @@ import { GuardService } from '../service/guard.service';
             margin-bottom: 10px;
         }
         .delivery-method{
-            margin-right: 50px;
+           margin: 20px 0;
         }
         button{
             margin: 12px 0;
@@ -45,77 +45,19 @@ import { GuardService } from '../service/guard.service';
     ]
 })
 export class CheckoutComponent {
-
-    // set default value for the form's inputs 
-    firstName: string = this.initializeInputWithDefaultValue("firstName");
-    lName: string = this.initializeInputWithDefaultValue("lastName");
-    address1: string = this.initializeInputWithDefaultValue("address1");
-    address2: string = this.initializeInputWithDefaultValue("address2");
-    pc: string = this.initializeInputWithDefaultValue("postCode");
-    email_: string = this.initializeInputWithDefaultValue("email");
-    phone_: string = this.initializeInputWithDefaultValue("phone");
-
-    defaultDeliveryMethod: string = this.initializeInputWithDefaultValue("deliveryMethod") || 'delivery';
-    deliveryMethods: string[] = [
-        'delivery', 'collection'
-    ]
-    constructor(public basket: BasketService, private router: Router, private guardService: GuardService) {
+    constructor(public basket: BasketService, private router: Router, private guardService: GuardService, public buyerDetailsService: BuyerDetailsService) {
 
     }
-
-    onSubmit(form: NgForm): void {
+    onSubmit(): void {
+        if (this.buyerDetailsService.selectedDeliveryMethod === 'Collection') {
+            this.buyerDetailsService.clearAddress()
+        }
         this.guardService.canGetPaymentRoute = true;
-        this.saveFormDetails(form.value);
         this.router.navigate(['./payment']);
     }
     preventDefault(event: Event): void {
         event.preventDefault();
     }
-
-    // get the customer detail from localStorage if it's available otherwise return empty string ""
-    private initializeInputWithDefaultValue(value: string) {
-        const storedDetails = JSON.parse(localStorage.getItem('checkout-details'));
-        return storedDetails ? storedDetails[value] : "";
-    }
-
-    onBlurMethod(value: any) {
-        // get the customer details from the local storage if they are already there
-        let storedInput = JSON.parse(localStorage.getItem('checkout-details'));
-
-        // if there is no stored details in the local storage set storedInput to empty object
-        if (!storedInput) {
-            storedInput = {};
-        }
-        // delete the stored input for replacing
-        storedInput[value.name] = value.model;
-        this.saveInputValue(storedInput)
-    }
-
-    onClickDeliveryMethod(value: any) {
-        let storedInput = JSON.parse(localStorage.getItem('checkout-details'));
-        if (!storedInput) {
-            storedInput = {};
-        }
-        storedInput.deliveryMethod = value;
-       this.saveInputValue(storedInput)
-    }
-
-    private saveFormDetails(formDetails: CheckoutForm) {
-        localStorage.setItem('checkout-details', JSON.stringify(formDetails));
-    }
-
-    private saveInputValue(value: any) {
-        localStorage.setItem('checkout-details', JSON.stringify(value));
-    }
 }
 
-interface CheckoutForm {
-    firstName: string;
-    lastName: string;
-    address1: string;
-    address2: string;
-    postCode: string;
-    email: string;
-    phone: string;
-    deliveryMethod: string;
-}
+
---
+++ front-end/app/service/buyer-details.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class BuyerDetailsService {
+    firstName = '';
+    lastName = '';
+    selectedDeliveryMethod = 'Delivery';
+    addressLine1 = '';
+    addressLine2 = '';
+    postcode = '';
+    email = '';
+    phone = '';
+    note = '';
+
+    deliveryMethods = [
+        'Delivery', 'Collection'
+    ];
+
+    clearAddress() {
+        this.addressLine1 = null;
+        this.addressLine2 = null;
+        this.postcode = null;
+    }
+}
