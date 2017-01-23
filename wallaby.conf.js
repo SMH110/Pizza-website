bc03e2c9256522f3df4809979c4f1e61f13a9f28
SMH110 <mahdihassan135@hotmail.com>
2017-01-23 18:29:57 +0000

    Add wallaby config - needs fixing

---
+++ wallaby.conf.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function(wallaby) {
+    process.env.NODE_PATH = process.env.NODE_PATH +
+        path.delimiter + path.join(wallaby.localProjectDir, 'back-end', 'node_modules') +
+        path.delimiter + path.join(wallaby.localProjectDir, 'front-end', 'node_modules');
+
+    return {
+        files: [
+            // Application code
+            { pattern: 'back-end/**/*.ts', load: false },
+            '!back-end/node_modules/**/*.ts',
+            { pattern: 'front-end/**/*.ts', load: false },
+            '!front-end/node_modules/**/*.ts',
+            { pattern: 'shared/**/*.ts', load: false }
+        ],
+        tests: [
+            'specs/unit/**/*.specs.ts'
+        ],
+        env: {
+            type: 'node'
+        },
+        testFramework: 'mocha'
+    };
+};
