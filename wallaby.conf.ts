b21a592e6a0a1ac80e82e87b585063c4aaaedb66
Massimo Hamilton <massimo.hamilton@outlook.com>
2017-02-16 16:43:12 +0000

    Convert Wallaby config to TypeScript

--- wallaby.conf.ts
+++ wallaby.conf.ts
@@ -1,8 +1,6 @@
-'use strict';
+import * as path from 'path';
 
-var path = require('path');
-
-module.exports = function (wallaby) {
+export = function (wallaby: any) {
     process.env.NODE_PATH = process.env.NODE_PATH +
         path.delimiter + path.join(wallaby.localProjectDir, 'back-end', 'node_modules') +
         path.delimiter + path.join(wallaby.localProjectDir, 'front-end', 'node_modules');
