56df366ddf13b81bc5ee9f069463f1f6adc3df95
SMH110 <mahdihassan135@hotmail.com>
2017-02-17 15:01:53 +0000

    Add BBQ base option

--- shared/business-rules/pizza-pricing-rule.ts
+++ shared/business-rules/pizza-pricing-rule.ts
@@ -3,10 +3,14 @@ import { PizzaToppingPrices } from '../static-data/toppings';
 
 export default function (item: BasketItem) {
     let price = calculateBasePrice(item);
+    const toppings = item.options.filter(option => option !== "BBQ base");
+    const isWithBBQ_Base: boolean = item.options.indexOf("BBQ base") !== - 1;
     if (item.name === 'Free Choice') {
-        price += Math.max(item.options.length - 5, 0) * PizzaToppingPrices[item.version];
+        price += Math.max(toppings.length - 5, 0) * PizzaToppingPrices[item.version];
+        price += isWithBBQ_Base ? 2 : 0;
     } else {
-        price += item.options.length * PizzaToppingPrices[item.version];
+        price += toppings.length * PizzaToppingPrices[item.version];
+        price += isWithBBQ_Base ? 2 : 0;
     }
     return price;
 }
