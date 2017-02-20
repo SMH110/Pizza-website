647ac30144e398cc96b5912b526b9cb589950271
SMH110 <mahdihassan135@hotmail.com>
2017-02-20 18:49:40 +0000

    Toppings for Potato skin #109

--- specs/unit/validations/item-options.specs.ts
+++ specs/unit/validations/item-options.specs.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { validateOrderRequest } from '../../../shared/validation/place-order-request-validator';
 import Catalogue from '../../../shared/static-data/catalogue';
 import { createValidOrders, PAYMENT_METHODS } from './order-helpers';
+import { OrderItemValidationObject } from '../../../shared/validation/place-order-request-validator';
 
 describe("Item options", () => {
     it("Can add valid toppings to a pizza", () => {
@@ -19,7 +20,7 @@ describe("Item options", () => {
     });
 
     it("Can't add options to items that don't support options", () => {
-        for (let itemType of ['salad', 'dessert', 'drink', 'calzone', 'pasta', 'side']) {
+        for (let itemType of ['salad', 'dessert', 'drink', 'calzone', 'pasta']) {
             for (let order of createValidOrders()) {
                 let item = Catalogue.find(x => x.tags.some(t => t === itemType));
                 order.orderItems.push({
@@ -34,4 +35,39 @@ describe("Item options", () => {
             }
         }
     });
+
+    it('Can add valid toppings to "Potato Skins with your favourite topping" side', () => {
+        for (let order of createValidOrders()) {
+            order.orderItems.push(createPotatoSkinSide());
+            expect(validateOrderRequest(order, PAYMENT_METHODS)).to.deep.equal([]);
+        }
+    });
+
+    it('Can not add "Potato Skins with your favourite topping" side without any topping', () => {
+        for (let order of createValidOrders()) {
+            order.orderItems.push(createPotatoSkinSide());
+            order.orderItems[1].options = [];
+            expect(validateOrderRequest(order, PAYMENT_METHODS)).to.deep.equal(["1 topping at least must be added to Potato Skins with your favourite topping"]);
+        }
+    });
+
+    it("Can't add invalid toppings to the 'Potato Skins with your favourite topping' side", () => {
+        for (let order of createValidOrders()) {
+            order.orderItems.push(createPotatoSkinSide());
+            order.orderItems[1].options.push("Blah blah")
+            expect(validateOrderRequest(order, PAYMENT_METHODS)).to.deep.equal(["Blah blah is not a valid topping for Potato Skins with your favourite topping"]);
+        }
+    });
 });
+
+function createPotatoSkinSide(): OrderItemValidationObject {
+    let potatoSkin = Catalogue.find(item => item.name === "Potato Skins with your favourite topping");
+    return {
+        name: potatoSkin.name,
+        options: ["Gorgonzola"],
+        price: potatoSkin.price as number,
+        quantity: 1,
+        tags: potatoSkin.tags,
+        version: null
+    }
+}
