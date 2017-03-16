162988abf6322ce1b710031b7743f79043f94426
SMH110 <mahdihassan135@hotmail.com>
2017-03-16 22:02:52 +0000

    Force the browser to use the resource from cache (#222)
    * Force the browser to use the resource from cache
    
    * Remove unnecessary if statement

--- back-end/app.ts
+++ back-end/app.ts
@@ -18,7 +18,10 @@ const app = express();
 const nocache = require('nocache');
 
 // Static Files
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use((_req, res, next) => {
+    res.setHeader('Cache-Control', 'public, max-age=31557600')
+    next();
+}, express.static(path.join(__dirname, '..', 'dist')));
 
 // JSON Requests
 app.use(bodyParser.json())
@@ -27,7 +30,7 @@ app.use(bodyParser.json())
 app.use('/api/order', nocache(), order);
 app.use('/api/admin', nocache(), admin);
 app.use('/api/payment', nocache(), payment);
-app.use('/health', health);
+app.use('/health', nocache(), health);
 app.use(nocache(), clientSide);
 
 // Register Payment Gateways
