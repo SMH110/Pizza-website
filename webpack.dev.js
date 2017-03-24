5e6968432973a19f5208d89344e49281bd494265
SMH110 <mahdihassan135@hotmail.com>
2017-03-24 09:15:46 +0000

    Fix webpack build

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,12 +3,13 @@ process.env.IS_TEST_ENVIRONMENT = "TRUE";
 const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const commonConfig = require('./webpack.common');
+const root = require('./webpack-helpers');
 
 module.exports = webpackMerge(commonConfig, {
     devtool: 'inline-source-map',
 
     output: {
-        path: './dist',
+        path: root('dist'),
         filename: '[name].js',
         chunkFilename: '[id].chunk.js'
     },
